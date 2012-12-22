22a49685157e3cef6012bc4c7cb3c79c52ac6abc
valerym <valeryf@wix.com>
2012-12-22 13:21:11 +0200

    - renamed wql expressions - added initial trait wqlparser - added initial trait wql2pig parser

--- src/main/scala/com/vf/wql2pig/Parser2.scala
+++ src/main/scala/com/vf/wql2pig/Parser2.scala
@@ -1,5 +1,9 @@
 package com.vf.wql2pig
 
+import util.parsing.input.CharSequenceReader
+import com.vf.wql2pig.definitions._
+import com.vf.pig.definitions.PigExpr
+
 
 /**
  * User: valeryf
@@ -7,11 +11,11 @@ package com.vf.wql2pig
  */
 
 trait WqlConstants extends util.parsing.combinator.RegexParsers {
-  val boolean: Parser[BooleanExpr] = ("true" | "false") ^^ {
-    s => BooleanExpr(s.toLowerCase.toBoolean)
+  val boolean: Parser[BooleanWqlExpr] = ("true" | "false") ^^ {
+    s => BooleanWqlExpr(s.toLowerCase.toBoolean)
   }
-  val integer: Parser[IntExpr] = """-?\d+""".r ^^ {
-    s => IntExpr(s.toInt)
+  val integer: Parser[IntWqlExpr] = """-?\d+""".r ^^ {
+    s => IntWqlExpr(s.toInt)
   }
 
   def unquote(s: String): String = {
@@ -20,68 +24,87 @@ trait WqlConstants extends util.parsing.combinator.RegexParsers {
     else s
   }
 
-  val string: Parser[StringExpr] = ("""'""" + """\w*""" + """'""").r ^^ {
-    s => StringExpr(unquote(s))
+  val string: Parser[StringWqlExpr] = ("""'""" + """\w*""" + """'""").r ^^ {
+    s => StringWqlExpr(unquote(s))
   }
-  val ident: Parser[VarExpr] = ("""[a-zA-Z_=*](\w|=)*""".r | failure("couldn't parse identifier")) ^^ {
-    s => VarExpr(s)
+  val ident: Parser[VarWqlExpr] = ("""[a-zA-Z_=*](\w|=)*""".r | failure("couldn't parse identifier")) ^^ {
+    s => VarWqlExpr(s)
   }
-  val const: Parser[LiteralExpr] = (boolean | integer | string | failure("couldn't parse constant"))
+  val const: Parser[LiteralWqlExpr] = (boolean | integer | string | failure("couldn't parse constant"))
 }
 
 trait WqlStatements extends WqlConstants {
   // order users by evid desc
-  val direction: Parser[OrderExpr] = ("asc" | "desc") ^^ {
-    case "asc" => OrderExpr("asc")
-    case "desc" => OrderExpr("desc")
+  val direction: Parser[OrderWqlExpr] = ("asc" | "desc") ^^ {
+    case "asc" => OrderWqlExpr("asc")
+    case "desc" => OrderWqlExpr("desc")
   }
 
-  def combine(column: VarExpr, direction: OrderExpr, s: List[VarExpr ~ OrderExpr]): List[(VarExpr, OrderExpr)] = {
+  def combine(column: VarWqlExpr, direction: OrderWqlExpr, s: List[VarWqlExpr ~ OrderWqlExpr]): List[(VarWqlExpr, OrderWqlExpr)] = {
     (column, direction) :: (s map (x => (x._1, x._2)))
   }
 
   val order: Parser[AbstractOrder] = ("order" ~ ident ~ "by" | "order" ~ "by") ~ ident ~ direction ~ (("," ~> ident ~ direction) *) ^^ {
     case "order" ~ "by" ~ column ~ dir ~ x =>
-      SelectOrderExpr(combine(column, dir, x))
-    case "order" ~ (relation: VarExpr) ~ "by" ~ column ~ dir ~ x =>
-      FullOrderExpr(relation, combine(column, dir, x))
+      SelectOrderWqlExpr(combine(column, dir, x))
+    case "order" ~ (relation: VarWqlExpr) ~ "by" ~ column ~ dir ~ x =>
+      FullOrderWqlExpr(relation, combine(column, dir, x))
   }
 
-  val join: Parser[JoinExpr] = ("join " ~> ident) ~ ("by" ~> ident) ~ (("," ~> ident ~ ("by" ~> ident)) *) ^^ {
+  val join: Parser[JoinWqlExpr] = ("join " ~> ident) ~ ("by" ~> ident) ~ (("," ~> ident ~ ("by" ~> ident)) *) ^^ {
     case table ~ column ~ rest => {
       val mapped = rest map (x => (x._1, x._2))
-      JoinExpr((table, column) :: mapped)
+      JoinWqlExpr((table, column) :: mapped)
     }
   }
 
-  val tableStatement: Parser[Expr] = order | select | join | filter
+  val tableStatement: Parser[WqlExpr] = order | select | join | filter
 
-  val assign: Parser[AssignExpr] = ident ~ "=" ~ tableStatement ^^ {
-    case relation ~ "=" ~ expr => AssignExpr(relation, expr)
-  }
-
-  val oper: Parser[OperExpr] = (const ~ ident ~ ident | ident ~ ident ~ const | "(" ~ oper ~ ")") ^^ {
-    case (left: Expr) ~ VarExpr(operator) ~ (right: Expr) => OperExpr(operator, left, right)
-    case "(" ~ (operand: OperExpr) ~ ")" => operand
-  }
-  val and: Parser[AndExpr] = (oper ~ "and" ~ condition) ^^ {
-    case cond1 ~ "and" ~ cond2 => AndExpr(cond1, cond2)
-  }
-  val or: Parser[OrExpr] = (oper ~ "or" ~ condition) ^^ {
-    case cond1 ~ "or" ~ cond2 => OrExpr(cond1, cond2)
-  }
-  val condition: Parser[ConditionExpr] = ((and | or | oper) | ("(" ~> condition <~ ")")) ^^ {
-    case cond: ConditionExpr => cond
+  val assign: Parser[AssignWqlExpr] = ident ~ "=" ~ tableStatement ^^ {
+    case relation ~ "=" ~ expr => AssignWqlExpr(relation, expr)
   }
 
-  val where: Parser[AbstractWhereExpr] = "where" ~> condition ^^ (cond => WhereExpr(cond))
-
-  val filter: Parser[FilterExpr] = ("filter" ~> ident) ~ ("by" ~> condition) ^^ {
-    case relation ~ conditions => FilterExpr(relation, conditions)
+  val oper: Parser[OperWqlExpr] = (const ~ ident ~ ident | ident ~ ident ~ const | "(" ~ oper ~ ")") ^^ {
+    case (left: WqlExpr) ~ VarWqlExpr(operator) ~ (right: WqlExpr) => OperWqlExpr(operator, left, right)
+    case "(" ~ (operand: OperWqlExpr) ~ ")" => operand
+  }
+  val and: Parser[AndWqlExpr] = (oper ~ "and" ~ condition) ^^ {
+    case cond1 ~ "and" ~ cond2 => AndWqlExpr(cond1, cond2)
+  }
+  val or: Parser[OrWqlExpr] = (oper ~ "or" ~ condition) ^^ {
+    case cond1 ~ "or" ~ cond2 => OrWqlExpr(cond1, cond2)
+  }
+  val condition: Parser[ConditionWqlExpr] = ((and | or | oper) | ("(" ~> condition <~ ")")) ^^ {
+    case cond: ConditionWqlExpr => cond
   }
 
-  val select: Parser[SelectExpr] = "select" ~ ident ~ "from" ~ ident ~ opt(where) ~ opt(order) ^^ {
-    case "select" ~ VarExpr(column) ~ "from" ~ relation ~ whereStmt ~ orderStmt =>
-      SelectExpr(ColumnsExpr(List(column)), relation, whereStmt.getOrElse(EmptyWhereExpr()), orderStmt.getOrElse(EmptyOrder()))
+  val where: Parser[AbstractWhereWqlExpr] = "where" ~> condition ^^ (cond => WhereWqlExpr(cond))
+
+  val filter: Parser[FilterWqlExpr] = ("filter" ~> ident) ~ ("by" ~> condition) ^^ {
+    case relation ~ conditions => FilterWqlExpr(relation, conditions)
+  }
+
+  val select: Parser[SelectWqlExpr] = "select" ~ ident ~ "from" ~ ident ~ opt(where) ~ opt(order) ^^ {
+    case "select" ~ VarWqlExpr(column) ~ "from" ~ relation ~ whereStmt ~ orderStmt =>
+      SelectWqlExpr(ColumnsWqlExpr(List(column)), relation, whereStmt.getOrElse(EmptyWhereWqlExpr()), orderStmt.getOrElse(EmptyOrder()))
+  }
+}
+
+trait WqlParser extends WqlStatements {
+  val wqlParser: Parser[List[WqlExpr]] = assign *
+
+  def parse(line: String): List[WqlExpr] = {
+    val input = new CharSequenceReader(line)
+    wqlParser(input) match {
+      case Success(result, _) => result
+      case Failure => throw new IllegalArgumentException("Could not parse " + line)
+    }
+  }
+}
+
+trait Wql2PigParser extends WqlParser {
+  def wql2pig(line: String): List[PigExpr] = {
+    val wqls = parse(line)
+    com.vf.pig.definitions.DumpExpr(com.vf.pig.definitions.VarExpr("dump")) :: Nil
   }
 }
