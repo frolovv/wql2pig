b245e45d247ea01a92bb3ecd3c3d19fd2bc959a9
valerym <valeryf@wix.com>
2012-12-24 01:30:47 +0200

    - added initial tests for Wql2Pig class

---
+++ src/test/scala/com/vf/pig/parser/Wql2PigTests.scala
@@ -0,0 +1,29 @@
+package com.vf.pig.parser
+
+import com.vf.pig.definitions._
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import com.vf.wql.parser.WqlParser
+import com.vf.pig.definitions.PigVar
+import com.vf.pig.definitions.PigForeach
+
+/**
+ * User: valeryf
+ * Date: 12/24/12 12:38 AM
+ */
+class Wql2PigTests extends Wql2Pig with ShouldMatchers with FlatSpec with WqlParser {
+  def parsing(line: String): List[Pig] = {
+    val wqls = super.parse(line)
+    pigify(wqls)
+  }
+
+  "Wql2Pig" should "pigify select statements" in {
+    val assign: PigAssign = PigAssign(PigVar("x"), PigForeach(PigVar("users"), List("evid"), PigSchema(List("evid"), List("long"))))
+    val filter: PigAssign = PigAssign(PigVar("x"), PigFilter(PigVar("x"), PigOper("=", PigVar("evid"), PigInt(4))))
+    val order: PigAssign = PigAssign(PigVar("x"), PigOrder(PigVar("x"), List((PigVar("evid"), PigDirection("desc"))), PigParallel(3)))
+
+    parsing("x = select evid from users") should equal(List(assign))
+    parsing("x = select evid from users where evid = 4") should equal(List(assign, filter))
+    parsing("x = select evid from users where evid = 4 order by evid desc") should equal(List(assign, filter, order))
+  }
+}
