3379863c8e4ce5a377c096d502af63744d1b0b42
valerym <valeryf@wix.com>
2013-01-02 00:47:55 +0200

    - added select clause test with wherekey and where clauses together

--- src/test/scala/com/vf/pig/parser/Wql2PigTests.scala
+++ src/test/scala/com/vf/pig/parser/Wql2PigTests.scala
@@ -22,18 +22,28 @@ class Wql2PigTests extends Wql2Pig with ShouldMatchers with FlatSpec with WqlPar
     val filter: PigAssign = PigAssign(PigVar("x"), PigFilter(PigVar("x"), PigOper("=", PigVar("evid"), PigInt(4))))
     val order: PigAssign = PigAssign(PigVar("x"), PigOrder(PigVar("x"), List((PigVar("evid"), PigDirection("desc"))), PigParallel(3)))
 
-    parsing("x = select evid from users wherekey src = 3 and date_created between('2012-15-16', '2012-16-18')") should equal(
-      List(PigAssign(PigVar("x"),
-        PigLoad(PigVar("wix-bi"), PigWixTableLoader("users", PigKeyFilter("2012-15-16", "2012-16-18", 3), PigColumnFilter(PigEmptyCondition()), List("evid")),
-          PigSchema(List("evid"), List("long"))))))
-
     parsing("x = select evid from users") should equal(List(assign))
     parsing("x = select evid from users where evid = 4") should equal(List(assign, filter))
     parsing("x = select evid from users where evid = 4 order by evid desc") should equal(List(assign, filter, order))
     parsing("x = select evid, uuid from users") should equal(List(PigAssign(PigVar("x"), PigForeach(PigVar("users"), List("evid", "uuid"), PigSchema(List("evid", "uuid"), List("long", "chararray"))))))
     parsing("x = select evid, uuid from users where evid = 4 order by evid desc") should equal(List(PigAssign(PigVar("x"), PigForeach(PigVar("users"), List("evid", "uuid"), PigSchema(List("evid", "uuid"), List("long", "chararray")))), filter, order))
 
+    parsing("x = select evid from users wherekey src = 3 and date_created between('2012-15-16', '2012-16-18')") should equal(
+      List(PigAssign(PigVar("x"),
+        PigLoad(PigVar("wix-bi"),
+          PigWixTableLoader("users", PigKeyFilter("2012-15-16", "2012-16-18", 3),
+            PigColumnFilter(PigEmptyCondition()),
+            List("evid")),
+          PigSchema(List("evid"), List("long"))))))
 
+    parsing("x = select evid from users wherekey src = 3 and date_created between('2012-15-16', '2012-16-18') where evid = 100") should equal(
+      List(PigAssign(PigVar("x"),
+        PigLoad(PigVar("wix-bi"),
+          PigWixTableLoader("users",
+            PigKeyFilter("2012-15-16", "2012-16-18", 3),
+            PigColumnFilter(PigOper("=", PigVar("evid"), PigInt(100))),
+            List("evid")),
+          PigSchema(List("evid"), List("long"))))))
   }
 
   it should "pigify order statements" in {
@@ -49,4 +59,6 @@ class Wql2PigTests extends Wql2Pig with ShouldMatchers with FlatSpec with WqlPar
     parsing("x = filter users by evid = 103") should
       equal(List(PigAssign(PigVar("x"), PigFilter(PigVar("users"), PigOper("=", PigVar("evid"), PigInt(103))))))
   }
+
+
 }
