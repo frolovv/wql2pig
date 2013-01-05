8272f6274e867802044a3a9bd41a49ade1811bc6
valerym <valeryf@wix.com>
2013-01-05 02:06:09 +0200

    - added wql2pig test of select with both group and where clauses

--- src/test/scala/com/vf/pig/parser/Wql2PigTests.scala
+++ src/test/scala/com/vf/pig/parser/Wql2PigTests.scala
@@ -58,10 +58,12 @@ class Wql2PigTests extends Wql2Pig with ShouldMatchers with FlatSpec with WqlPar
 
   it should "pigify select statements with group" in {
     parsing("x = select evid from users group by evid") should equal(
-      List(
-        PigAssign(PigVar("x"), PigGroup(PigVar("users"), List("evid"), PigParallel(3))),
-        PigAssign(PigVar("x"), PigForeach(PigVar("x"), List("evid"), PigSchema(List("evid"), List("long"))))
-        )
+      List(PigAssign(PigVar("x"), PigGroup(PigVar("users"), List("evid"), PigParallel(3))),
+        PigAssign(PigVar("x"), PigForeach(PigVar("x"), List("evid"), PigSchema(List("evid"), List("long"))))))
+    parsing("x = select evid from users where evid = 100 group by evid") should equal(
+      List(PigAssign(PigVar("x"), PigFilter(PigVar("users"), PigOper("=", PigVar("evid"), PigInt(100)))),
+        PigAssign(PigVar("x"), PigGroup(PigVar("x"), List("evid"), PigParallel(3))),
+        PigAssign(PigVar("x"), PigForeach(PigVar("x"), List("evid"), PigSchema(List("evid"), List("long")))))
     )
   }
 
